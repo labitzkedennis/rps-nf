5cd672b01abb4a6b44d1802d039d987af6baa09a
Dennis Labitzke <dennis@dlabitzke.de>
2025-07-29 20:01:57 +0200

    Add tests for player behaviour
    * Assume there is an enum "Move" representing the possible moves per player per round
    * Test players returning their names
    * Test players making their moves, either random or only paper, based on player type

---
+++ src/test/java/de/labitzke/rps_nf/player/PlayerTest.java
@@ -0,0 +1,48 @@
+package de.labitzke.rps_nf.player;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PlayerTest {
+
+
+    @Test
+    public void playersReturningTheirNames() {
+        Player paperPlayer = new PaperPlayer("A");
+        Assertions.assertEquals("A", paperPlayer.name());
+
+        Player randomPlayer = new RandomPlayer("B");
+        Assertions.assertEquals("B", randomPlayer.name());
+    }
+
+    @Test
+    public void paperPlayerAlwaysPlayingPaper() {
+        Player paperPlayer = new PaperPlayer("PP");
+        IntStream.range(0,100).forEach(i -> Assertions.assertEquals(Move.PAPER, paperPlayer.makeMove()));
+    }
+
+    @Test
+    public void randomPlayerPlaysSomehowRandomly() {
+        int numberOfRounds = 100000;
+        Player randomPlayer = new RandomPlayer("RP");
+        List<Move> randomPlayerMoves = IntStream.range(0, numberOfRounds).mapToObj(i -> randomPlayer.makeMove()).toList();
+
+        Set<Map.Entry<Move, Long>> moves = randomPlayerMoves.stream()
+                .collect(Collectors.groupingBy(v -> v, Collectors.counting()))
+                .entrySet();
+
+        Assertions.assertEquals(Move.values().length, moves.size());
+
+        moves.forEach((entry) -> Assertions.assertAll(
+                () -> Assertions.assertNotEquals(0, entry.getValue()),
+                () -> Assertions.assertNotEquals(numberOfRounds, entry.getValue())
+        ));
+    }
+
+}
